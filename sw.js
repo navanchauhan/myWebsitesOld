e25bbe11e90e47fadf04f3a8be2b5827498f140b
Navan Chauhan <navanchauhan@gmail.com>
2018-02-09 21:53:21 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,6 +9,11 @@ caches.open('Navan Chauhan').then(function(cache) {
 return cache.addAll([
 
 '/css/menu.css',
+  '/',
+  'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css',
+  'https://www.w3schools.com/lib/w3.js',
   
   '/js/menu.js',
   'css/style.css',
