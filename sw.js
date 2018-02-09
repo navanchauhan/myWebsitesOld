c998d3b25002d982a61f540eb8a950837bf2ab73
Navan Chauhan <navanchauhan@gmail.com>
2018-02-09 21:12:15 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -35,4 +35,14 @@ self.addEventListener('fetch', function(event) {
 
 console.log(event.request.url);
 
+event.respondWith(
+
+caches.match(event.request).then(function(response) {
+
+return response || fetch(event.request);
+
+})
+
+);
+
 });
